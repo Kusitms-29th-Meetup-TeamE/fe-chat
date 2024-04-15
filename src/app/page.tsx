9271c187e030e9ee70fea6238b073816ffe1b2ab
chamny20 <dge3179@ajou.ac.kr>
2024-04-15 16:35:25 +0900

    test: test page routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       frontend-chat practice
+      <button
+        className="border rounded-md"
+        onClick={() => router.push("/test")}
+      >
+        chating page로 가기
+      </button>
     </main>
   );
 }
---
+++ src/app/test/page.tsx
@@ -0,0 +1,3 @@
+export default function Test() {
+  return <>hihi This page is Chat-page.</>;
+}
